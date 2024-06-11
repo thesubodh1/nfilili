6ca26324fa49f2ac5f4d2e10c09381300e6e12a2
thesubodh1 <thesubodh1@gmail.com>
2024-06-11 14:10:48 +0545

    ui changes made

---
+++ nfilili/static/nfilili/js/script.js
@@ -0,0 +1,28 @@
+const timer = document.querySelector("#timer")
+
+let deadline = new Date("jun 12, 2024 12:30:00").getTime();
+
+let x = setInterval(function () {
+
+    let now = new Date().getTime();
+
+    let t = deadline - now;
+
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+        (t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor(
+        (t % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor(
+        (t % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML =
+        days + "d " + hours + "h " + 
+        minutes + "m " + seconds + "s ";
+
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("timer")
+                .innerHTML = "EXPIRED";
+    }
+}, 1000);
